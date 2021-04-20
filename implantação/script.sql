6d7e7e43ce249a92ab7e83a463af2f182d660b9b
Matthew Nascimento <matthewromie@hotmail.com>
2021-04-20 13:46:44 -0300

    front-end

--- implantação/script.sql
+++ implantação/script.sql
@@ -1,3 +1,6 @@
+-- DROP SCHEMA dbo;
+
+CREATE SCHEMA dbo;
 -- lab02.dbo.Agente definition
 
 -- Drop table
@@ -12,7 +15,7 @@ CREATE TABLE lab02.dbo.Agente (
 	Nome varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Placa_Veiculo varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	CONSTRAINT PK_Agente PRIMARY KEY (CNPJ)
-);
+) GO;
 
 
 -- lab02.dbo.Automovel definition
@@ -28,7 +31,7 @@ CREATE TABLE lab02.dbo.Automovel (
 	Marca varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	Modelo varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	CONSTRAINT PK_Veiculo PRIMARY KEY (Placa)
-);
+) GO;
 
 
 -- lab02.dbo.Contratante definition
@@ -42,14 +45,14 @@ CREATE TABLE lab02.dbo.Contratante (
 	RG varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Endereco varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Profissao varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	Entidades_Empregadoras varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Entidades_Empregadoras nchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Rendimento float NOT NULL,
 	Login varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Senha varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Nome varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Placa_Veiculo varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	CONSTRAINT PK_Contratante PRIMARY KEY (CPF)
-);
+) GO;
 
 
 -- lab02.dbo.Pedido definition
@@ -59,13 +62,13 @@ CREATE TABLE lab02.dbo.Contratante (
 -- DROP TABLE lab02.dbo.Pedido GO
 
 CREATE TABLE lab02.dbo.Pedido (
-	Id_Pedido INTEGER NOT NULL,
+	Id_Pedido INTEGER AUTO_INCREMENT NOT NULL,
 	CPF_Contratante varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	Placa_Veiculo varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	CONSTRAINT PK_Pedido_1 PRIMARY KEY (Id_Pedido),
 	CONSTRAINT FK_Pedido_Automovel FOREIGN KEY (Placa_Veiculo) REFERENCES lab02.dbo.Automovel(Placa),
 	CONSTRAINT FK_Pedido_Contratante FOREIGN KEY (CPF_Contratante) REFERENCES lab02.dbo.Contratante(CPF)
-);
+) GO;
 
 
 -- lab02.dbo.Parecer definition
@@ -75,12 +78,12 @@ CREATE TABLE lab02.dbo.Pedido (
 -- DROP TABLE lab02.dbo.Parecer GO
 
 CREATE TABLE lab02.dbo.Parecer (
-	Id_Parecer INTEGER NOT NULL,
-	Id_Pedido INTEGER NOT NULL,
+	Id_Parecer INTEGER AUTO_INCREMENT NOT NULL,
+	Id_Pedido INTEGER AUTO_INCREMENT NOT NULL,
 	CNPJ_Agente varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	Aprovado bit NOT NULL,
 	[Data] date NOT NULL,
 	CONSTRAINT PK_Parecer PRIMARY KEY (Id_Parecer),
 	CONSTRAINT FK_Parecer_Agente FOREIGN KEY (CNPJ_Agente) REFERENCES lab02.dbo.Agente(CNPJ),
 	CONSTRAINT FK_Parecer_Pedido FOREIGN KEY (Id_Pedido) REFERENCES lab02.dbo.Pedido(Id_Pedido)
-)
+) GO;
